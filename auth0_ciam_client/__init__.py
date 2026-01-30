53cc4aaaa16662d78f6da977669d7ef849f2ea0f
Brian Patrick Reavey <brian@reavey05.com>
2026-01-30 09:49:39 -0500

    Add AuthResult and NetworkError to __init__.py exports

--- auth0_ciam_client/__init__.py
+++ auth0_ciam_client/__init__.py
@@ -5,24 +5,27 @@ __author__ = "Brian Reavey"
 __email__ = "brian@reavey05.com"
 __license__ = "GPL-3.0-only"
 
-from .core import AuthenticationCore
+from .core import AuthenticationCore, AuthResult
 from .session_store import SessionStore, InMemorySessionStore
 from .exceptions import (
     AuthenticationError,
     AuthenticationRequiredError,
     SessionExpiredError,
     InvalidCredentialsError,
+    NetworkError,
 )
 from .config import Auth0Config, create_angel_studios_config
 
 __all__ = [
     "AuthenticationCore",
+    "AuthResult",
     "SessionStore",
     "InMemorySessionStore",
     "AuthenticationError",
     "AuthenticationRequiredError",
     "SessionExpiredError",
     "InvalidCredentialsError",
+    "NetworkError",
     "Auth0Config",
     "create_angel_studios_config",
 ]
